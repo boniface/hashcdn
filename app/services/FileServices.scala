8df3af955d83a35623e95c44c9146034fa3f30c8
hashcode <boniface@hashcode.zm>
2015-10-05 20:09:25 +0200

    Added Post and get Controllers

--- app/services/FileServices.scala
+++ app/services/FileServices.scala
@@ -1,8 +1,11 @@
 package services
 
-import java.io.{FileInputStream, File}
+import java.io.{ByteArrayInputStream, FileInputStream, File}
 
 import com.mongodb.casbah.gridfs.GridFSDBFile
+import com.sksamuel.scrimage.ScaleMethod.{Bicubic, FastScale}
+import com.sksamuel.scrimage.nio.{PngWriter, GifWriter, JpegWriter}
+import com.sksamuel.scrimage.{Position, FormatDetector, Format, Image}
 import domain.{FileMeta, FileResults}
 import repository.FilesRepository
 
@@ -16,10 +19,8 @@ object FileServices {
       FilesRepository.getFileById(id)
   }
 
-
   def processFile(data: File, meta: FileMeta):Future[Seq[FileResults]] = {
     import scala.concurrent.ExecutionContext.Implicits.global
-
     meta.contentType.startsWith("image") match {
       case true => Future {
         val r = FileResults("1","2",None)
@@ -43,4 +44,30 @@ object FileServices {
     FileMeta("","")
   }
 
+  def createThumb(file: File):File={
+    val format = FormatDetector.detect(new FileInputStream(file))
+
+    format match {
+      case Some(Format.GIF) => {Image.fromFile(file).fit(180,120).output(file)((GifWriter()))}
+      case Some(Format.JPEG)=> Image.fromFile(file).fit(180,120).output(file)((JpegWriter()))
+      case Some(Format.PNG)=> Image.fromFile(file).fit(180,120).output(file)((PngWriter()))
+      case _ =>Image.fromFile(file).fit(180,120).output(file)
+    }
+  }
+
+  def createImage(file: File):File={
+    val format = FormatDetector.detect(new FileInputStream(file))
+    val out = file
+    format match {
+      case Some(Format.GIF) => { println(" GIF CALLED ")
+        Image.fromFile(file).scaleTo(650,500).output(file)((GifWriter()))}
+      case Some(Format.JPEG)=> { println(" JPEG CALLED ")
+        Image.fromFile(out).scale(0.5, Bicubic).output(out)}
+      case Some(Format.PNG)=> { println(" PNG CALLED ")
+        Image.fromFile(file).scaleTo(650,500).output(file)((PngWriter()))}
+      case _ => { println(" DEFAULT CALLED ")
+        Image.fromFile(file).scaleTo(650,500).output(file)
+      }
+    }
+  }
 }
--- test/services/FileTypesTest.scala
+++ test/services/FileTypesTest.scala
@@ -1,8 +1,9 @@
 package services
 
-import java.io.{FileInputStream, File}
+import java.io.{FileOutputStream, FileInputStream, File}
+import javax.imageio.ImageIO
 
-import org.junit.Test
+import org.junit.{Ignore, Test}
 
 
 /**
@@ -10,14 +11,21 @@ import org.junit.Test
  */
 class FileTypesTest {
   @Test
-  def testFileType(): Unit ={
-    val  url = getClass().getResource("image.jpg")
-    val  file = new File(url.getPath())
+  def testFileType(): Unit = {
+
+    val url = getClass().getResource("image.jpg")
+    val file = new File(url.getPath())
     val logo = new FileInputStream(file)
+    //    val mimeType = FileTypeService.detectFile("http://localhost:9000/cdn/56095b6ebe24baa41da57f13")
 
-    val mimeType = FileTypeService.detectFile("http://localhost:9000/cdn/56095b6ebe24baa41da57f13")
+    val outfile = FileServices.createImage(file)
+//    val outfileThumb = FileServices.createThumb(file)
+    val  image = ImageIO.read(outfile)
+//    val  imageb = ImageIO.read(outfileThumb)
+    ImageIO.write(image, "jpg",new File("/home/hashcode/hashprojects/hashcdn/test/services/hederle.jpg"))
+//    ImageIO.write(imageb, "jpg",new File("/home/hashcode/hashprojects/hashcdn/test/services/thumb.jpg"))
 
-    println("The Results is ",mimeType)
+
 
   }
 
